073435290fb82b628356df6f989133002110e1c5
simonaszz <simasak56@gmail.com>
2023-05-04 00:43:16 +0300

    admin dashboard filed up

--- src/resources/views/admin/admin_dashboard.blade.php
+++ src/resources/views/admin/admin_dashboard.blade.php
@@ -6,23 +6,24 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!--favicon-->
-    <link rel="icon" href="assets/images/favicon-32x32.png" type="image/png" />
+    <link rel="icon" href="{{ asset('adminbackend/assets/images/favicon-32x32.png') }}" type="image/png" />
     <!--plugins-->
-    <link href="assets/plugins/vectormap/jquery-jvectormap-2.0.2.css" rel="stylesheet" />
-    <link href="assets/plugins/simplebar/css/simplebar.css" rel="stylesheet" />
-    <link href="assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css" rel="stylesheet" />
-    <link href="assets/plugins/metismenu/css/metisMenu.min.css" rel="stylesheet" />
+    <link href="{{ asset('adminbackend/assets/plugins/vectormap/jquery-jvectormap-2.0.2.css') }}" rel="stylesheet" />
+    <link href="{{ asset('adminbackend/assets/plugins/simplebar/css/simplebar.css') }}" rel="stylesheet" />
+    <link href="{{ asset('adminbackend/assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css') }}"
+        rel="stylesheet" />
+    <link href="{{ asset('adminbackend/assets/plugins/metismenu/css/metisMenu.min.css') }}" rel="stylesheet" />
     <!-- loader-->
-    <link href="assets/css/pace.min.css" rel="stylesheet" />
-    <script src="assets/js/pace.min.js"></script>
+    <link href="{{ asset('adminbackend/assets/css/pace.min.css') }}" rel="stylesheet" />
+    <script src="{{ asset('adminbackend/assets/js/pace.min.js') }}"></script>
     <!-- Bootstrap CSS -->
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
-    <link href="assets/css/app.css" rel="stylesheet">
-    <link href="assets/css/icons.css" rel="stylesheet">
+    <link href="{{ asset('adminbackend/assets/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('adminbackend/assets/css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('adminbackend/assets/css/icons.css') }}" rel="stylesheet">
     <!-- Theme Style CSS -->
-    <link rel="stylesheet" href="assets/css/dark-theme.css" />
-    <link rel="stylesheet" href="assets/css/semi-dark.css" />
-    <link rel="stylesheet" href="assets/css/header-colors.css" />
+    <link rel="stylesheet" href="{{ asset('adminbackend/assets/css/dark-theme.css') }}" />
+    <link rel="stylesheet" href="{{ asset('adminbackend/assets/css/semi-dark.css') }}" />
+    <link rel="stylesheet" href="{{ asset('adminbackend/assets/css/header-colors.css') }}" />
     <title>Rukada - Responsive Bootstrap 5 Admin Template</title>
 </head>
 
@@ -1636,27 +1637,28 @@
     </div>
     <!--end switcher-->
     <!-- Bootstrap JS -->
-    <script src="assets/js/bootstrap.bundle.min.js"></script>
+
+    <script src="{{ asset('adminbackend/assets/js/bootstrap.bundle.min.js') }}"></script>
     <!--plugins-->
-    <script src="assets/js/jquery.min.js"></script>
-    <script src="assets/plugins/simplebar/js/simplebar.min.js"></script>
-    <script src="assets/plugins/metismenu/js/metisMenu.min.js"></script>
-    <script src="assets/plugins/perfect-scrollbar/js/perfect-scrollbar.js"></script>
-    <script src="assets/plugins/chartjs/js/Chart.min.js"></script>
-    <script src="assets/plugins/vectormap/jquery-jvectormap-2.0.2.min.js"></script>
-    <script src="assets/plugins/vectormap/jquery-jvectormap-world-mill-en.js"></script>
-    <script src="assets/plugins/jquery.easy-pie-chart/jquery.easypiechart.min.js"></script>
-    <script src="assets/plugins/sparkline-charts/jquery.sparkline.min.js"></script>
-    <script src="assets/plugins/jquery-knob/excanvas.js"></script>
-    <script src="assets/plugins/jquery-knob/jquery.knob.js"></script>
+    <script src="{{ asset('adminbackend/assets/js/jquery.min.js') }}"></script>
+    <script src="{{ asset('adminbackend/assets/plugins/simplebar/js/simplebar.min.js') }}"></script>
+    <script src="{{ asset('adminbackend/assets/plugins/metismenu/js/metisMenu.min.js') }}"></script>
+    <script src="{{ asset('adminbackend/assets/plugins/perfect-scrollbar/js/perfect-scrollbar.js') }}"></script>
+    <script src="{{ asset('adminbackend/assets/plugins/chartjs/js/Chart.min.js') }}"></script>
+    <script src="{{ asset('adminbackend/assets/plugins/vectormap/jquery-jvectormap-2.0.2.min.js') }}"></script>
+    <script src="{{ asset('adminbackend/assets/plugins/vectormap/jquery-jvectormap-world-mill-en.js') }}"></script>
+    <script src="{{ asset('adminbackend/assets/plugins/jquery.easy-pie-chart/jquery.easypiechart.min.js') }}"></script>
+    <script src="{{ asset('adminbackend/assets/plugins/sparkline-charts/jquery.sparkline.min.js') }}"></script>
+    <script src="{{ asset('adminbackend/assets/plugins/jquery-knob/excanvas.js') }}"></script>
+    <script src="{{ asset('adminbackend/assets/plugins/jquery-knob/jquery.knob.js') }}"></script>
     <script>
         $(function() {
             $(".knob").knob();
         });
     </script>
-    <script src="assets/js/index.js"></script>
+    <script src="{{ asset('adminbackend/assets/js/index.js') }}"></script>
     <!--app JS-->
-    <script src="assets/js/app.js"></script>
+    <script src="{{ asset('adminbackend/assets/js/app.js') }}"></script>
 </body>
 
 </html>
