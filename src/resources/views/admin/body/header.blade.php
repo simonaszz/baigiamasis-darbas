50026069cfc32d790456406a4e439d5036b2869b
simonaszz <simasak56@gmail.com>
2023-05-05 16:56:28 +0300

    admin template update

--- src/resources/views/admin/body/header.blade.php
+++ src/resources/views/admin/body/header.blade.php
@@ -382,8 +382,8 @@
                             <img src="{{ asset('adminbackend/assets/images/avatars/avatar-2.png') }}" class="user-img"
                                 alt="user avatar">
                             <div class="user-info ps-3">
-                                <p class="user-name mb-0">Pauline Seitz</p>
-                                <p class="designattion mb-0">Web Designer</p>
+                                <p class="user-name mb-0">{{ Auth::user()->name }}</p>
+                                <p class="designattion mb-0">{{ Auth::user()->username }}</p>
                             </div>
                         </a>
                         <ul class="dropdown-menu dropdown-menu-end">
