6901f64682ec3ce66968889d38c746c362b95da9
simonaszz <simasak56@gmail.com>
2023-05-05 12:26:23 +0300

    admin blade separated

--- src/app/Http/Controllers/AdminController.php
+++ src/app/Http/Controllers/AdminController.php
@@ -3,11 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\RedirectResponse;
 
 class AdminController extends Controller
 {
     public function AdminDashboard()
     {
-        return view('admin.admin_dashboard');
+        return view('admin.index');
+    } //End Method
+
+
+    public function AdminDestroy(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
     } //End Method
 }
--- src/routes/web.php
+++ src/routes/web.php
@@ -33,10 +33,21 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__ . '/auth.php';
 
+
+
+//Admin Dashboard
+
 Route::middleware(['auth', 'role:0'])->group(function () {
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('
 admin.dashboard');
+    Route::get('/admin/logout', [AdminController::class, 'AdminDestroy'])->name('
+admin.logout');
 });
+
+
+
+
+//Vendor Dashboard
 Route::middleware(['auth', 'role:1'])->group(function () {
 
     Route::get('/vendor/dashboard', [VendorController::class, 'VendorDashboard'])->name('
