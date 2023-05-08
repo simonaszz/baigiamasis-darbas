5a69b41a09c6539e1b0ff2b69cfed9b84c7ba61d
simonaszz <simasak56@gmail.com>
2023-05-08 12:37:20 +0300

    vendor account created and ful set up like admin profile

--- src/routes/web.php
+++ src/routes/web.php
@@ -58,12 +58,20 @@ Route::middleware(['auth', 'role:0'])->group(function () {
 
 //Vendor Dashboard
 Route::middleware(['auth', 'role:1'])->group(function () {
+    Route::get('/vendor/dashboard', [VendorController::class, 'VendorDashboard'])->
+        name('vendor.dashboard');
 
-    Route::get('/vendor/dashboard', [VendorController::class, 'VendorDashboard'])->name('
-vendor.dashboard');
+    Route::get('/vendor/logout', [VendorController::class, 'VendorDestroy'])->name('vendor.logout');
+
+    Route::get('/vendor/profile', [VendorController::class, 'VendorProfile'])->name('vendor.profile');
+
+    Route::post('/vendor/profile/store', [VendorController::class, 'VendorProfileStore'])->name('vendor.profile.store');
+
+    Route::get('/vendor/change/password', [VendorController::class, 'VendorChangePassword'])->name('vendor.change.password');
+
+    Route::post('/vendor/update/password', [VendorController::class, 'VendorUpdatePassword'])->name('vendor.update.password');
+
 });
 
-
-
-
 Route::get('/admin/login', [AdminController::class, 'AdminLogin']);
+Route::get('/vendor/login', [VendorController::class, 'VendorLogin']);
